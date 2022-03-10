b4bf700a34083f99dfa037989dbcb949259a4c71
[Avneesh Jha] <[avneeshjha2015@gmail.com]>
2022-03-10 23:59:44 +0530

    float and string

--- services/emojifier/api.py
+++ services/emojifier/api.py
@@ -24,7 +24,7 @@ with open('services/emojifier/glove.6B.50d.txt',encoding='utf-8') as f:
         coeffs = np.asarray(values[1:])
         #coeffs = np.asarray(values[1:],dtype='float32')
         
-        embeddings[word] = coeffs
+        embeddings[word] = float(coeffs)
 
 def getOutputEmbeddings(X):
     
